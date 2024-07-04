64a95e6d7c579dc1fb24af75593694da6ded1a85
pedro-batista <peddrobatista2022@gmail.com>
2024-07-04 15:09:43 -0300

    Primeiro commit projeto criado

---
+++ src/test/java/com/peddrobatista/workshopmongo/JavamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peddrobatista.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
