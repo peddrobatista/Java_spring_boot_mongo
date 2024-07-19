fff4204109b72666026eea8eb79befda57ee8254
pedro-batista <peddrobatista2022@gmail.com>
2024-07-19 19:30:01 -0300

    Consulta simples com query methods

--- src/main/java/com/peddrobatista/workshopmongo/services/PostService.java
+++ src/main/java/com/peddrobatista/workshopmongo/services/PostService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.peddrobatista.workshopmongo.domain.Post;
-import com.peddrobatista.workshopmongo.domain.User;
-import com.peddrobatista.workshopmongo.dto.UserDTO;
 import com.peddrobatista.workshopmongo.repository.PostRepository;
 import com.peddrobatista.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
